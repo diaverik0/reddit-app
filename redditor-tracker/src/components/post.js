c5f3239c3d617f46bbd78b569bf60aa139903f07
diaverik <diaverik0@gmail.com>
2022-11-21 07:32:11 -0800

    added formating to the Post component

--- redditor-tracker/src/components/post.js
+++ redditor-tracker/src/components/post.js
@@ -3,9 +3,9 @@ export const Post = ({item}) => {
     return (
       <li style={{border: 'solid 1px', margin: '10px'}}>
         <a href={item.imgUrl}><img style={{height:'10%'}}src={item.img} alt={item.id}/></a>
-        <p>{item.title}</p>
+        <h3>{item.title}</h3>
         <p>ups: {item.upVotes}</p>
-        <p>{item.content}</p>
+        <p style={{fontSize: '10px'}}>{item.content}</p>
         </li>
     )
 }
